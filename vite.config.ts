7b002a9bf8caea01c26bf96ffc05044997e10e62
khang-dao <khangcs@outlook.com>
2024-09-10 20:35:52 +1000

    chore: update config

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,16 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   server: {
-    port: 3000
+    port: 3000,
+    // proxy: {
+    //   '/api': {
+    //     target: 'http://localhost:8080/api',
+    //     changeOrigin: true,
+    //     rewrite: (path) => path.replace(/^\/api/, '')
+    //   }
+    // }
   },
   resolve: {
     alias: {
