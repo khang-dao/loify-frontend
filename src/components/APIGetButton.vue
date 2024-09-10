8bf704dc141e2316766afdfac650b2162e23f305
khang-dao <khangcs@outlook.com>
2024-09-10 20:36:14 +1000

    feat: implement basic buttons to fetch successfully from loify-backend

---
+++ src/components/APIGetButton.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import axios from 'axios'
+
+const { url } = defineProps(['url'])
+
+const getData = async () => {
+  const response = await axios.get(url)
+  console.log(response.data)
+}
+</script>
+
+<template>
+  <button @click="getData">
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped></style>
---
+++ src/components/APIPostButton.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import axios from 'axios'
+
+const { url, requestBody } = defineProps(['url', 'requestBody'])
+
+const postData = async () => {
+  const response = await axios.post(url, requestBody)
+  console.log(response.data)
+}
+</script>
+
+<template>
+  <button @click="postData">
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped></style>
