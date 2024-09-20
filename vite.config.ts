b8f24f57378b27d873a4d274dffc3bdcd85987dc
khang-dao <86513733+khang-dao@users.noreply.github.com>
2024-09-20 19:29:21 +1000

    feat: basic fetch from backend (#4)
    * test: add whitespace
    
    * chore: update dependencies
    
    * chore: include vite folder in gitignore
    
    * chore: update dev port to 3000
    
    * chore: update deps
    
    * chore: update config
    
    * feat: implement basic buttons to fetch successfully from loify-backend
    
    * feat: allow basic fetch from backend

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,17 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
+  server: {
+    port: 3000,
+    // proxy: {
+    //   '/api': {
+    //     target: 'http://localhost:8080/api',
+    //     changeOrigin: true,
+    //     rewrite: (path) => path.replace(/^\/api/, '')
+    //   }
+    // }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
