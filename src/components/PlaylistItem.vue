825b67981c89d22a8c8635be8e1d91409c9ed4b2
khang-dao <khangcs@outlook.com>
2024-09-20 20:55:47 +1000

    feat: add column components and items

---
+++ src/components/PlaylistItem.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const { playlistName, imgSrc, imgAlt="Cannot load image" } = defineProps<{
+  playlistName: string
+  artistName: string
+  imgSrc: string
+  imgAlt?: string
+}>()
+</script>
+
+<template>
+  <div class="container">
+    <img :src="imgSrc" :alt="imgAlt" width="100" height="100"/>
+    <h3>{{ playlistName }}</h3>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  gap: 1.5rem;
+  align-items: center;
+}
+</style>
