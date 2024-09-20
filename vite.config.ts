51affc7c145624300ec364a4b1715a49da1f972c
khang-dao <86513733+khang-dao@users.noreply.github.com>
2024-09-20 19:28:36 +1000

    Revert "test: add whitespace (#2)"
    This reverts commit 56b02f2ccb701f939a4614f1991003518473a16c.

--- vite.config.ts
+++ vite.config.ts
@@ -5,17 +5,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    port: 3000,
-    // proxy: {
-    //   '/api': {
-    //     target: 'http://localhost:8080/api',
-    //     changeOrigin: true,
-    //     rewrite: (path) => path.replace(/^\/api/, '')
-    //   }
-    // }
-  },
+  plugins: [
+    vue(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
