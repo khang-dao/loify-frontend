45d46fbb910947a1e2b0cf3b6442a30c842e4381
khang-dao <khangcs@outlook.com>
2024-09-29 18:26:17 +1000

    feat: add styling to home & login page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,12 +3,39 @@
 </script>
 
 <template>
-  <main>
-    <h1>welcome to loify 🍃</h1>
-    <a href="/login">click to begin</a>
+  <main class="main">
+    <div class="container">
+      <h1 class="heading">welcome to loify 🍃</h1>
+      <a class="subheading" href="/login">click to begin</a>
+    </div>
   </main>
 </template>
 
 <style scoped>
+.main {
+  background: #232323;
+  height: 98vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
 
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+  font-family: 'League Spartan', sans-serif;
+}
+
+.heading {
+  color: #FFFFFF;
+  font-size: 2rem;
+}
+
+.subheading {
+  text-decoration: none;
+  color: #6A6A6A;
+  font-size: 1rem;
+}
 </style>
