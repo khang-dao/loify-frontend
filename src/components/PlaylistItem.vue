33927ceb907adc57b1fb39b8cedc3ec579f0a669
khang-dao <khangcs@outlook.com>
2024-09-29 23:02:24 +1000

    feat: add styling to main loify page

--- src/components/PlaylistItem.vue
+++ src/components/PlaylistItem.vue
@@ -9,8 +9,8 @@ const { playlistId, playlistName, imgSrc, imgAlt="Cannot load image", selected} 
 </script>
 
 <template>
-  <div :class="['container', { selected: selected }]" :id="playlistId">    <!-- TODO: Set the `playlistId` in some other way -->
-    <img :src="imgSrc" :alt="imgAlt" width="100" height="100" :id="playlistId"/>    <!-- TODO: Set the `playlistId` in some other way -->
+  <div class="container" :id="playlistId">    <!-- TODO: Set the `playlistId` in some other way -->
+    <img :class="[ { selected: selected }]" :src="imgSrc" :alt="imgAlt" width="100" height="100" :id="playlistId"/>    <!-- TODO: Set the `playlistId` in some other way -->
     <h3 :id="playlistId">{{ playlistName }}</h3>    <!-- TODO: Set the `playlistId` in some other way -->
   </div>
 </template>
@@ -20,13 +20,20 @@ const { playlistId, playlistName, imgSrc, imgAlt="Cannot load image", selected} 
   display: flex;
   gap: 1.5rem;
   align-items: center;
+  min-height: min-content; 
 }
 
 h3 {
   flex-grow: 1;
+  font-family: 'League Spartan', sans-serif;
+  color: #FFFFFF
 }
 
 .selected {
-  border: 1px solid rgb(169, 112, 208);
+  border: 4px solid rgb(169, 112, 208);
+}
+
+img {
+  border-radius: 0.5rem;
 }
 </style>
--- src/views/LoifyView.vue
+++ src/views/LoifyView.vue
@@ -5,9 +5,6 @@ import TrackItem from '@/components/TrackItem.vue'
 import axios from 'axios'
 import { ref, reactive, watchEffect, onMounted } from 'vue';
 
-// const showLoifyedTracks = ref(false)
-
-// const generateLoifyedTracks = () => showLoifyedTracks.value = !showLoifyedTracks.value
 
 const selectedPlaylist = ref(null)
 
@@ -46,26 +43,18 @@ async function fetchPlaylists() {
     playlists.images = playlistsData.map((p) => p.images?.[0].url)
 }
 
-// async function fetchTracks() { 
-//     const url = `http://localhost:8080/api/spotify/playlists/${selectedPlaylist.value}/tracks`
-//     const response = await axios.get(url)
-//     const tracksData = response.data.items
+async function fetchTracks() { 
+    const url = `http://localhost:8080/api/spotify/playlists/${selectedPlaylist.value}/tracks`
+    const response = await axios.get(url)
+    const tracksData = response.data.items
 
-//     tracks.names = tracksData.map((t) => t.track.name)
-//     tracks.images = tracksData.map((t) => t.track.album.images?.[0].url)
-//     tracks.artists = tracksData.map((t) => t.track.artists?.[0].name)
-// }
+    tracks.names = tracksData.map((t) => t.track.name)
+    tracks.images = tracksData.map((t) => t.track.album.images?.[0].url)
+    tracks.artists = tracksData.map((t) => t.track.artists?.[0].name)
+}
 
 watchEffect(async () => {
-  const url = `http://localhost:8080/api/spotify/playlists/${selectedPlaylist.value}/tracks`
-  const response = await axios.get(url)
-  const tracksData = response.data.items
-  
-  tracks.names = tracksData.map((t) => t.track.name)
-  tracks.images = tracksData.map((t) => t.track.album.images?.[0].url)
-  tracks.artists = tracksData.map((t) => t.track.artists?.[0].name)
-
-  console.log("currenlty selected playlist: " + selectedPlaylist.value)
+  fetchTracks()
 })
 
 async function fetchLoifyedTracks() {
@@ -92,19 +81,19 @@ onMounted(() => fetchPlaylists())
 </script>
 
 <template>
-  <main>
+  <main class="main">
     <div class="column column-1">
-      <h2>P L A Y L I S T S</h2>
+      <h2 class="col-heading">P L A Y L I S T S</h2>
       <PlaylistItem v-for="(name, index) in playlists.names" @click="selectPlaylist" :selected="selectedPlaylist === playlists.id[index]" :playlistId="playlists.id[index]" :key="index" :playlistName="name" :imgSrc="playlists.images[index]"/>
     </div>
 
     <div class="column column-2">
-      <h2>S O N G S</h2>
+      <h2 class="col-heading">S O N G S</h2>
       <TrackItem v-for="(name, index) in tracks.names" :key="index" :trackName="name" :artistName="tracks.artists[index]" :imgSrc="tracks.images[index]"/>
     </div>
 
     <div class="column column-3">
-      <h2>S O N G S 🍃</h2>
+      <h2 class="col-heading">S O N G S 🍃</h2>
       <button @click="fetchLoifyedTracks">Generate Loifyed Songs 🍃</button>
       <button @click="createLoifyedPlaylist">Create new playlist with loifyed songs 💚</button>
       <TrackItem v-for="(name, index) in loifyedTracks.names" :key="index" :trackName="name" :artistName="loifyedTracks.artists[index]" :imgSrc="loifyedTracks.images[index]"/>
@@ -113,25 +102,42 @@ onMounted(() => fetchPlaylists())
 </template>
 
 <style scoped>
-main {
+.main {
   display: flex;
+  background: #232323;
+  height: 100vh;
+  overflow: auto;
+  flex:1
+}
+
+.col-heading {
+  font-family: 'League Spartan', sans-serif;
+  color: #FFFFFF;
+  text-align: center;
 }
 
 .column {
   display: flex;
   flex-direction: column;
   flex: 1;
-  gap: 1rem;
-  padding: 1rem
+  gap: 3rem;
+  padding: 1rem;
+  overflow: auto;
 }
 
-.button-wrapper {
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
+::-webkit-scrollbar {
+  width: 0.7rem;
 }
 
-.wrapper {
-
+::-webkit-scrollbar-track {
+  background: #232323;
+  margin-top: 5rem;
+  margin-bottom: 1rem;
 }
+
+::-webkit-scrollbar-thumb {
+  background-color: #3b3b3b;
+  border-radius: 1rem;
+}
+
 </style>
