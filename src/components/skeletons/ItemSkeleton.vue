6dcadb08b3d4aff754b4cb3b97b409ee4a4ad1f5
khang-dao <khangcs@outlook.com>
2024-10-07 17:53:16 +1100

    feat: create naive skeleton component

---
+++ src/components/skeletons/ItemSkeleton.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="container">
+    <div class="picture"></div>
+
+    <div class="text-container">
+      <h3>...</h3>
+      <p>...</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  gap: 1.5rem;
+  align-items: center;
+}
+
+.picture {
+  border-radius: 0.5rem;
+  background-color: grey;
+  height: 100px;
+  width: 100px;
+}
+
+.text-container {
+  display: flex;
+  flex-direction: column;
+  font-family: 'League Spartan', sans-serif;
+  color: #6a6a6a;
+  gap: 0.4rem;
+}
+
+img {
+  border-radius: 0.5rem;
+}
+</style>
