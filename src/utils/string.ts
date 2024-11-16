3072edb1ad80177f22434b4ca8a9afb483db7429
khang-dao <khangcs@outlook.com>
2024-11-16 17:24:08 +1100

    refactor: bring util fn to outer scope

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,9 +1,9 @@
 export const containsLoify = (str: string) => str.toLowerCase().includes("loify");
 export const addSpacesBetweenChars = (str: string) => str.split('').join(' ');
+const nonAlphaNum = (str: string) => /^[^\w\s]/.test(str);
 
 // Utility function to sort by name, putting emoji-starting items at the end
 export const customSort = (arr) => {
-  const nonAlphaNum = (str) => /^[^\w\s]/.test(str);
   return arr.sort((a, b) => {
     const aIsEmoji = nonAlphaNum(a.name);
     const bIsEmoji = nonAlphaNum(b.name);
