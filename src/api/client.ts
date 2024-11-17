edfd94216dd074369b254a36b7e59166116e0d12
khang-dao <khangcs@outlook.com>
2024-11-17 12:22:19 +1100

    setup: config for env variables

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,19 +1,10 @@
 import axios from 'axios';
+import config from '../config'
 
 const client = axios.create({
-  baseURL: 'http://localhost:8080/api/v1',  // TODO: get this from .env file
+  baseURL: config.apiBaseUrl,
   withCredentials: true,
 });
 
 
 export default client;
-
-
-// TODO: consider this
-// constants/api.js
-// export const BASE_URL = "http://localhost:8080/api/v1";
-// export const API_ENDPOINTS = {
-//   playlists: `${BASE_URL}/me/playlists`,
-//   playlistTracks: (id) => `${BASE_URL}/playlists/${id}/tracks`,
-//   loifyPlaylist: (id) => `${BASE_URL}/playlists/${id}/loify?genre=lofi`,
-// };
