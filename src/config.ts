1614e4cf18971ad3be39a8a4f131af3d1556c6ab
khang-dao <86513733+khang-dao@users.noreply.github.com>
2024-12-15 18:54:56 +1100

    chore: update api routes and minor styling changes (#38)
    * chore: update api route naming andf refs
    
    * chore: update bcm button styling
    
    * chore: update skeleton size

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ interface Config {
 }
 
 const config: Config = {
-  apiBaseUrl: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080/api/v1'
+  apiBaseUrl: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080/v1'
 }
 
 export default config
