eb8f26998ba46126e0e63307034c59061d937a9e
khang-dao <khangcs@outlook.com>
2024-12-20 18:38:59 +1100

    chore: format

--- src/components/misc/LoadingSpinner.vue
+++ src/components/misc/LoadingSpinner.vue
@@ -5,18 +5,18 @@
   
 <style scoped>
 .spinner {
-width: 40px;
-height: 40px;
-border: 4px solid rgba(0, 0, 0, 0.1);
-border-top-color: #ffffff;
-border-radius: 50%;
-animation: spin 1s linear infinite;
+  width: 40px;
+  height: 40px;
+  border: 4px solid rgba(0, 0, 0, 0.1);
+  border-top-color: #ffffff;
+  border-radius: 50%;
+  animation: spin 1s linear infinite;
 }
 
 @keyframes spin {
-to {
-    transform: rotate(360deg);
-}
+  to {
+      transform: rotate(360deg);
+  }
 }
 </style>
   
