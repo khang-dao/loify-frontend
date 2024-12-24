f5c5e34eb31ad9f91732e5a95662f9cf8225cfca
khang-dao <khangcs@outlook.com>
2024-12-24 13:03:54 +1100

    fix: type err

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,14 +1,19 @@
+import { Playlist } from "@/types"
+
 export const containsLoify = (str: string) => str.toLowerCase().includes('loify')
 export const addSpacesBetweenChars = (str: string) => str.split('').join(' ')
 const nonAlphaNum = (str: string) => /^[^\w\s]/.test(str)
 
 // Custom sort: 'loify' items at the start & emoji items at the end
-export const customSort = (arr) => {
+export const customSort = (arr: Playlist[]): Playlist[] => {
   return arr.sort((a, b) => {
-    const aIsLoify = a.name.includes('loify')
-    const bIsLoify = b.name.includes('loify')
-    const aIsEmoji = nonAlphaNum(a.name)
-    const bIsEmoji = nonAlphaNum(b.name)
+    const aName = a.name ?? '';
+    const bName = b.name ?? '';
+    
+    const aIsLoify = aName.includes('loify')
+    const bIsLoify = bName.includes('loify')
+    const aIsEmoji = nonAlphaNum(aName)
+    const bIsEmoji = nonAlphaNum(bName)
 
     // Prioritize "loify" elements first, in the order they appear
     if (aIsLoify && !bIsLoify) return -1
@@ -19,6 +24,6 @@ export const customSort = (arr) => {
     if (bIsEmoji && !aIsEmoji) return -1
 
     // Regular alphabetical sorting
-    return a.name.localeCompare(b.name)
+    return aName.localeCompare(bName)
   })
 }
